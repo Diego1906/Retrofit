8797b1980e42250e4d2fb4551c47d7f6c4843aec
Diego <di_fsantos@yahoo.com.br>
2020-02-12 16:11:40 -0300

    Arquivo "MarsApiService" - Foi feita a implementação da classe Moshi que será usada pelo retrofit. - Foi substituída a chamada do conversor ScalarsConverterFactory pelo MoshiConverterFactory na criação do objeto retrofit. - Foi alterado o tipo de retorno da função "getProperties", agora a mesma passou a retorna uma lista de objeto.
    Arquivo MarsProperty
    - Foi feita a implementação do data class MarsProperty.
    
    Arquivo OverviewViewModel
    - No corpo da função "getMarsRealEstateProperties" nos lugares onde era usada a classe String foi alterado para usar List<MarsProperty>.
    
    Arquivo build.gradle (Module:app)
    - Foi feita a implementação dos artefatos necessários para utilização da classe Moshi e do converter-moshi do retrofit.
    - Foi removida a implementação do artefato converter-scalars do retrofit.

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,19 +17,29 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
+/*
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
 /**
-* Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
-* object pointing to the desired URL
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object pointing to the desired URL
  */
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
@@ -43,7 +53,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    fun getProperties(): Call<String>
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 /**
