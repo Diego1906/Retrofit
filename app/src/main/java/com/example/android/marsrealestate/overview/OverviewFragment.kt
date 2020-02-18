04f8250c61327660d16ff4de5119e8d345f784ff
Diego <di_fsantos@yahoo.com.br>
2020-02-18 15:39:49 -0300

    Na classe "OverviewFragment" - Foi implementada a configuração do adapter do RecyclerView - Foi criado um observador para o campo status do ViewModel
    Na classe "OverviewViewModel"
    - Foi alterado nome da propriedade de "response" para "status"
    - Foi criada uma nova propriedade chamada properties do tipo MutableLiveData<List<MarsProperty>>
    - Na função "getMarsRealEstateProperties" foi a atribuição do retorno do serviço do retrofit para a list de objetos.
    
    No arquivo "PhotoGridAdapter"
    - Foi feita a implementação do adapter do RecyclerView.
    - Dentro da classe "PhotoGridAdapter" que foi criada existe uma inner class para o ViewHolder e um Companion Object para implementação da Interface DiffUtil.ItemCallback<MarsProperty>
    
    No arquivo BindingAdapters
    - Foi feita a implementação de dois BindingAdapters que serão utilizados direto no XML do layout.
    
    No arquivo "fragment_overview"
    - Foi substituido o TextView pelo RecyclerView.
    
    No arquivo "grid_view_item"
    - Foi implementada uma variável do tipo MarsProperty que será preenchida pela ViewHolder dentro do Adapter do RecyclerView.
    - O elemento imageView "mars_image", passou a receber a Url direto da variável property.
    
    No arquivo build.gradle module
    - Foram implementados dois novos artefatos um para a lib do Glide e a outra para RecyclerView.

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -1,25 +1,10 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.marsrealestate.overview
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
@@ -50,7 +35,17 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
+        // Sets the adapter of the photosGrid RecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        viewModel.status.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                Toast.makeText(activity, it, Toast.LENGTH_LONG).show()
+            }
+        })
+
         setHasOptionsMenu(true)
+
         return binding.root
     }
 
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -1,26 +1,10 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.marsrealestate.overview
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.*
 
 /**
@@ -34,12 +18,20 @@ class OverviewViewModel : ViewModel() {
     // The Coroutine runs using the Main (UI) dispatcher
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    // The internal MutableLiveData String that stores the most recent response status
+    private val _status = MutableLiveData<String>()
 
-    // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    // The external immutable LiveData for the status String
+    val status: LiveData<String>
+        get() = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
+    // with new values
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+
+    // The external LiveData interface to the properties is immutable, so only this class can modify
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -49,18 +41,20 @@ class OverviewViewModel : ViewModel() {
     }
 
     /**
-     * Sets the value of the response LiveData to the Mars API status or the successful number of
-     * Mars properties retrieved.
+     * Gets Mars real estate property information from the Mars API Retrofit service and updates the
+     * [MarsProperty] [List] [LiveData]. The Retrofit service returns a List<MarsProperty>
      */
     private fun getMarsRealEstateProperties() {
         coroutineScope.launch {
-
+            // this will run on a thread managed by coroutines
             withContext(Dispatchers.IO) {
-                val properties = MarsApi.retrofitService.getProperties()
                 try {
-                    _response.postValue("Success: ${properties.size} Mars properties retrieved")
+                    // Get the List<MarsProperty> object for our Retrofit request
+                    val listResult = MarsApi.retrofitService.getProperties()
+                    _status.postValue("Success: ${listResult.size}")
+                    _properties.postValue(listResult)
                 } catch (ex: Exception) {
-                    _response.postValue("Failure: ${ex.message}")
+                    _status.postValue("Failure: ${ex.message}")
                 }
             }
         }
